bbfd821ee6235040224a30f077cf388d92757a48
wuyunzhu <wyzhdu@163.com>
2024-09-22 21:39:49 +0800

    feat: update sh

--- post-github.sh
+++ post-github.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
-# npm run build
+npm run build
 
 
 
-# output_dir="./build_file"
-# rm -rf $output_dir
-# mkdir $output_dir
-# search_dir="./packages/"
+output_dir="./build_file"
+rm -rf $output_dir
+mkdir $output_dir
+search_dir="./packages/"
 
-# for package in `ls $search_dir`; do
-#     package_dist="$search_dir$package/dist/";
-#     cp -r $package_dist $output_dir
-# done
-
-
-
-# git stash
-# git branch -D gh-pages
-# git fetch -p 
-# git checkout -b gh-pages origin/gh-pages
+for package in `ls $search_dir`; do
+    package_dist="$search_dir$package/dist/";
+    cp -r $package_dist $output_dir
+done
 
 
-# for file in ./*; do
-#     # echo $file;
-#     if [[ "$file" != "./post-github.sh" &&
-#           "$file" != "./build_file" &&
-#           "$file" != "./packages" &&
-#           "$file" != "./node_modules"
-#     ]]; then
-#         rm -r "$file"
-#     fi
-# done
+
+git stash
+git branch -D gh-pages
+git fetch -p 
+git checkout -b gh-pages origin/gh-pages
 
 
-# mv $output_dir/* ./
-# rm -rf $output_dir
+for file in ./*; do
+    # echo $file;
+    if [[ "$file" != "./post-github.sh" &&
+          "$file" != "./build_file" &&
+          "$file" != "./packages" &&
+          "$file" != "./node_modules"
+    ]]; then
+        rm -r "$file"
+    fi
+done
+
+
+mv $output_dir/* ./
+rm -rf $output_dir
 
 git add .
 git commit -m "feat: update blog"
